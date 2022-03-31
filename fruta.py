60c6a3d723a3758fbff620abf62174277c5f8495
seb4ru <sebasscho@pm.me>
2022-03-31 09:11:52 -0500

    arreglo de 1. ingreso de datos 2. impresión de resultados 3. orden de juego

--- fruta.py
+++ fruta.py
@@ -9,37 +9,64 @@ def ingreso_de_datos():
     # jugadores_nombres = nombres de los jugadores
 
     jugadores = int(input("Ingrese la cantidad de jugadores:"))
-    lanzamiento_i = [random.randint(1, (30 * 1)) for _ in range(jugadores)]
-    jugadores_nombres = []
-    contador_while = jugadores
-    print("\n")
+    lanzamiento_i = []
+
+    while(len(lanzamiento_i) < jugadores):
+
+        j = random.randint(1, (30 * 1))
+            
+        if not(j in lanzamiento_i):
+            
+            lanzamiento_i.append(j)
+
+        jugadores_nombres = []
+        contador_while = jugadores
+    
     
     while(contador_while != 0):
         
         jugadores_nombres.append(input("A continuación ingresará uno por uno el nombres de cada jugador: "))
+        print('\n')
         contador_while = contador_while - 1
 
     return(jugadores, lanzamiento_i, jugadores_nombres)
 
 datos_basicos = ingreso_de_datos()
-print(datos_basicos[0])
-print(datos_basicos[1])
 print(datos_basicos)       
 
 #----IMPRESIÓN DE RESULTADOS----
 
 
 for i in range(1, datos_basicos[0]+1):
-    print(datos_basicos[2][i-1],' sacó', datos_basicos[1][i-1],'  en el puntaje inicial de dados', "\n")
+      print('\n',datos_basicos[2][i-1],' sacó', datos_basicos[1][i-1],'  en el puntaje inicial de dados')
 
 #----ORDEN DE JUEGO----
 
 
-        # aquí meter un diccionario
+    # aquí meter un diccionario para el orden de los jugadores
 
-ordenLanzamiento = sorted(datos_basicos[1], reverse=True)
+ordenLanzamiento = datos_basicos[1]
+ordenLanzamiento.sort(reverse = True)
+orden_lanzamiento = {}
 
-print('El orden de juego será el siguiente (dado por el puntaje obtenido en el lanzamiento inicial de dados)):', ordenLanzamiento, '\n')
+print(ordenLanzamiento)
+
+
+for i in range(0, len(ordenLanzamiento)):
+    
+    orden_lanzamiento[ordenLanzamiento[i]] = datos_basicos[2][i-1]
+
+print(orden_lanzamiento, '\n')
+
+print('El orden de juego será el siguiente (dado por el puntaje obtenido en el lanzamiento inicial de dados)):', '\n')
+
+x = 1
+
+
+for i in range(1, datos_basicos[0] + 1):
+    
+    print('Jugador ', x ,orden_lanzamiento[ordenLanzamiento[i-1]])
+    x = x + 1
 
 print('¡AHORA SÍ, EMPECEMOS!')
 
@@ -81,7 +108,7 @@ def lanzamientos(jugadores, valorInicial):
     mesa = jugadores * valorInicial
     
     print('\nMesa: ', mesa)
-    print('Primer jugador\n')
+    print('Jugador\n', )
     
     lanzamiento = random.randint(1, (6 * 1))
     

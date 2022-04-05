95e1d5b4617780a9c991e32dfc25ba2cc534ebf8
seb4ru <sebasscho@pm.me>
2022-04-04 21:35:42 -0500

    arreglo de unas vainas dentro del while

--- fruta.py
+++ fruta.py
@@ -1,4 +1,3 @@
-from code import interact
 import random
 
 #----SOLICITAR CANTIDAD DE JUGADORES----
@@ -13,11 +12,9 @@ def ingreso_de_datos():
     lanzamiento_i = []
 
     while(len(lanzamiento_i) < jugadores):
-
         j = random.randint(1, (30 * 1))
             
-        if not(j in lanzamiento_i):
-            
+        if not(j in lanzamiento_i):            
             lanzamiento_i.append(j)
 
         jugadores_nombres = []
@@ -27,33 +24,25 @@ def ingreso_de_datos():
     while(contador_while != 0):
         
         jugadores_nombres.append(input("A continuación ingresará uno por uno el nombres de cada jugador: "))
-        print('\n')
         contador_while = contador_while - 1
 
     return(jugadores, lanzamiento_i, jugadores_nombres)
 
 datos_basicos = ingreso_de_datos()
-print(datos_basicos)       
+#print(datos_basicos)       
 
 #----ORDEN DE JUEGO----
-
-
-    # aquí meter un diccionario para el orden de los jugadores
 
 ordenLanzamiento = datos_basicos[1]
 ordenLanzamiento.sort(reverse = True)
 orden_lanzamiento = {}
 
-for i in range(0, len(ordenLanzamiento)):
-    
+for i in range(0, len(ordenLanzamiento)):    
     orden_lanzamiento[ordenLanzamiento[i]] = datos_basicos[2][i-1]
 
-print(orden_lanzamiento, '\n')
 
 valorInicial = float(input('Ingrese valor de la apuesta inicial acordado por todos los participantes : '))
-
-print('\n¡AHORA SÍ, EMPECEMOS!\n')
-print('El orden de juego será el siguiente (dado por el puntaje obtenido en el lanzamiento inicial de dados)):', '\n')
+print('\n¡AHORA SÍ, EMPECEMOS!\n El orden de juego será el siguiente (dado por el puntaje obtenido en el lanzamiento inicial de dados)):', '\n')
 print('\n')
 
 x = 1
@@ -67,108 +56,148 @@ print('\n')
 print("Total de jugadores:", datos_basicos[0])
 print("Apuesta inicial:", valorInicial)
 
-#----IMPRESIÓN DE RESULTADOS----
-
 
 #----CONDICIONES DEL JUEGO----
 
-
-mesa = datos_basicos[0] * valorInicial
-    
+mesa = datos_basicos[0] * valorInicial    
 print('\nMesa: ', mesa)
-print('Jugador\n', )
     
-
-    
-def lanzamientos():
-    
+def lanzamientos():    
     return (random.randint(1, 6)) # Caras del dado
     
-
-
+#---JUEGO---
 contador_porcentual = 0
 
-
-print(orden_lanzamiento)
-
 del ordenLanzamiento
-
 ordenLanzamiento = []
-
 for i in orden_lanzamiento:
     ordenLanzamiento.append(orden_lanzamiento[i])
 
+casino = 0
+nueva_mesa = 0
+ganancias = 0
+ganancias1 = 0
 
-print(ordenLanzamiento,'\n')
-
-
-
-while(mesa != 0):
+while(mesa > 0):
 
     jugador_actual = datos_basicos[2][ contador_porcentual % datos_basicos[0]-1]
-    print('Jugador actual: ', jugador_actual)
+    print('JUGADOR ACTUAL: ', jugador_actual)
     print(' El valor actual en la mesa es: ', mesa)
 
     lanzamiento_actual = lanzamientos()
 
     if not( lanzamiento_actual == 1 or lanzamiento_actual == 6 ):
+
+        print(' Dado obtenido en el primer lanzamiento: ', lanzamiento_actual)
+        decisición = input(' ¿Desea retractarse de su apuesta y pasar al turno siguiente jugador? Y/N: ')
         
-        print(' Dado obtenido: ', lanzamiento_actual)
-    
+        if (decisición in ["n","N"]):
+        
+            nueva_apuesta = input(' ¿Desea apostar por un número mayor o un número menor? MAY/MEN: ')
+            
+            while(nueva_mesa < valorInicial):
+
+                nueva_mesa = float(input(' \nIngrese el nuevo valor a apostar. Recuerde que no puede ser una cantidad menor a la inicial: '))
+
+            mesa = mesa + nueva_mesa #Aquí se le suma a la mesa el valor que el jugador apostó
+            print(' El valor actual en la mesa es: ', mesa)
+            segundo_lanzamiento = lanzamientos()            
+            
+            if (nueva_apuesta == 'MAY'):
+        
+                    if (segundo_lanzamiento > lanzamiento_actual):
+        
+                        print(' Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' El jugador gana, retira lo que aposto')
+                       
+                        ganancias1 = mesa * .05
+                        casino = casino + ganancias1
+                        mesa = mesa-(ganancias1 + nueva_mesa)    
+                       
+                        print ('Ganancias del casino:' , casino)
+                        print(' Nuevo valor en la mesa: ', mesa, '\n')   
+
+                    elif (segundo_lanzamiento < lanzamiento_actual): 
+                        
+                        print('Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' ',jugador_actual, 'pierde su dinero')
+                        print(' Nuevo valor en la mesa', mesa, '\n')      
+                    
+                    else:
+                    
+                        print('Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' ',jugador_actual, 'pierde turno por lanzar dos tiros iguales y pierde su dinero')
+                        print(' Nuevo valor en la mesa', mesa, '\n') 
+            
+            elif (nueva_apuesta == 'MEN'):
+            
+                    if (segundo_lanzamiento < lanzamiento_actual):
+
+                        print(' Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' El jugador gana, retira lo que aposto')
+                        
+                        ganancias = mesa*0.95
+                        casino = casino + ganancias 
+                        mesa = mesa - ( casino + nueva_mesa )
+                        
+                        print ('Las ganancias del casino son:' , casino)
+                        print(' Nuevo valor en la mesa: ', mesa, '\n')  
+                    
+                    elif (segundo_lanzamiento > lanzamiento_actual): 
+
+                        print('Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' ',jugador_actual, 'pierde su dinero')
+                        print(' Nuevo valor en la mesa', mesa, '\n')     
+                    
+                    else:
+                    
+                        print(' Dado obtenido en el segundo lanzamiento: ', segundo_lanzamiento)
+                        print(' ',jugador_actual, 'pierde turno por lanzar dos tiros iguales y pierde su dinero')
+                        print(' Nuevo valor en la mesa', mesa, '\n') 
+        
+        elif decisición == 'Y':
+        
+                print('SIGUIENTE JUGADOR')            
+                continue
+
     elif (lanzamiento_actual == 1):
-        
-        print(' Dado obtenido: ', lanzamiento_actual)
-        print(' ',jugador_actual, 'pierde turno y da a la mesa el valor de la apuesta inicial')
-        
-        mesa = mesa + valorInicial
-        print(' Nuevo valor en la mesa', mesa, '\n')
-    
+
+            print(' Dado obtenido: ', lanzamiento_actual)
+            print(' ',jugador_actual, 'pierde turno y da a la mesa el valor de la apuesta inicial')
+            mesa = mesa + valorInicial
+            print ('Las ganancias del casino son: ', casino)
+            print(' Nuevo valor en la mesa', mesa, '\n')
+
     else:
 
-        print(' Dado obtenido: ', lanzamiento_actual)
-        print(' ',jugador_actual, 'pierde turno pero saca de la mesa el equivalente al valor inicial de la apuesta')
-
-        mesa = mesa -(valorInicial + mesa*0.05)
-        print(' El nuevo valor en la mesa es: ', mesa, '\n')
-    
-    # este input es para que el código en el momento se pare para ver los datos obtenidos o algo así, luego se quita y se modifica o algo gdygkl
-    input(" pasar al siguiente jugador")
+            print(' Dado obtenido: ', lanzamiento_actual)
+            print(' ',jugador_actual, 'pierde turno pero saca de la mesa el equivalente al valor inicial de la apuesta')
+            mesa = mesa -((2*valorInicial) + mesa*0.05)
+            ganancias2 = mesa - mesa*0.95
+            casino = casino+ganancias2
+            print(' El nuevo valor en la mesa es: ', mesa, '\n')
+            print ('Las ganancias del casino son: ', casino)
+                                    
+    if (mesa <= 0):
+        mesa = 0
+        
     print('\n')
 
     contador_porcentual = contador_porcentual + 1
 
+    # Cierre del cilco while
+        
 
 
+print('LA MESA HA QUEDADO CON $0. JUEGO TERMINADO')
+print ('GANANCIAS TOTALES DEL CASINO: ', casino)
 
 
+#def historial():
+#Función que muestra el historial
+#....
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#historial = print(¿Desea conocer el historial de juego? Y/N): ---Aquí se pregunta si quiere conocer o no el historial---
+#    if historial = 'Y':
+#        historial()
+#    else:
